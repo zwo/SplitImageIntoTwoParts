27c21c0f1ec83d8c2a9c5818042f3823cc1d0fd4
zwo <zwo28@163.com>
2013-05-18 10:49:00 +0800

    first commit

---
+++ UIImage+SplitImageIntoTwoParts.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+SplitImageIntoTwoParts.h
+//  TapRepublic
+//
+//  Created by Weiou ZHOU on 13-5-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (SplitImageIntoTwoParts)
++ (NSArray*)splitImageIntoTwoParts:(UIImage*)image;
+@end
